328f22ab130eec3b85457611257f579050912a88
Tomaž Tekavec <tomaz@codurance.com>
2015-11-17 11:08:03 +0000

    Leap year kata (single commit).

---
+++ LeapYear.Tests/YearShould.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace LeapYear.Tests
+{
+    [TestFixture]
+    public class YearShould
+    {
+        [Test] public void BeALeapYearIfItIsDivisibleBy4()
+        {
+            var year = new Year(2016);
+
+            Assert.That(year.IsLeap(), Is.True);
+        }
+
+        [Test]
+        public void NotBeALeapYearIfItIsNotDivisibleBy4()
+        {
+            var year = new Year(2015);
+
+            Assert.That(year.IsLeap(), Is.False);
+        }
+
+        [Test]
+        public void BeALeapYearIfItIsDivisibleBy400()
+        {
+            var year = new Year(4000);
+
+            Assert.That(year.IsLeap(), Is.True);
+        }
+
+        [Test]
+        public void NotBeALeapYearIfItIsDivisibleBy100AndNotDivisibleBy400()
+        {
+            var year = new Year(4100);
+
+            Assert.That(year.IsLeap(), Is.False);
+        }
+
+    }
+}
---
+++ LeapYear/Year.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeapYear
+{
+    public class Year
+    {
+        private readonly int _year;
+
+        public Year(int year)
+        {
+            _year = year;
+        }
+
+        public bool IsLeap()
+        {
+            return IsDivisibleBy(100) ? IsDivisibleBy(400) : IsDivisibleBy(4);
+        }
+
+        private bool IsDivisibleBy(int value)
+        {
+            return _year%value == 0;
+        }
+    }
+}
